90547b54fc0aca5a8bcf4c23f5bc6f2f5840d8af
Akshay Joshi <akshayjoshi1117@gmail.com>
2022-06-18 05:52:30 +0530

    initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import logo from "../../images/logo.png";
+
+const Footer = () => (
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+    
+
+    <div className="flex justify-center items-center flex-col mt-5">
+      <p className="text-white text-sm text-center">Made as a personal project by</p>
+      <p className="text-white text-sm text-center font-medium mt-2">Akshay Joshi</p>
+    </div>   
+  </div>
+);
+
+export default Footer;
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+
+import abi from "./Transactions.json"
+
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xea92Cf80a844AEE9014a5fc632B9616a452d326A"
+
